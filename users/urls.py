0956a3cd35beed9dfcd71b9459b008c127d28eb1
peter <wchegesalome@gmail.com>
2024-08-27 12:08:23 -0400

    commit 1

---
+++ users/urls.py
@@ -0,0 +1,17 @@
+"""Defines URL patterns for users"""
+
+from django.urls import path, include
+from django.contrib.auth.views import LogoutView
+
+
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    #Include default auth urls
+    path('', include('django.contrib.auth.urls')),
+    # registration page
+    path('register/', views.register, name='register'),
+    # logout user
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
